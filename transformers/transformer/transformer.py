bd360d80492b8112ba2a6fd48d46778ebe904cb1
proshanto-c <proshantochanda@gmail.com>
2025-07-05 16:47:15 +0100

    transformer template

--- transformers/transformer/transformer.py
+++ transformers/transformer/transformer.py
@@ -6,15 +6,29 @@ from transformers.transformer.mhsa import MultiHeadSelfAttention
 import numpy as np
 
 class Transformer(nn.Module):
-    def __init__(self, seq_len, d_model):
+    def __init__(self, seq_len, d_model, n_enc_blocks, n_dec_blocks, n_out):
         super().__init__()
         self.seq_len = seq_len
         self.d_model = d_model
+        self.n_out = n_out
+        self.encoder_blocks = nn.ModuleList([EncoderBlock() for _ in range(n_enc_blocks)])
+        self.decoder_blocks = nn.ModuleList([DecoderBlock() for _ in range(n_dec_blocks)])
+        self.final_linear = nn.Linear(d_model, n_out)
+        self.sm = nn.Softmax()
 
     def forward(self, x):
         PE = get_positional_encoding(self.seq_len, self.d_model)
         x = x + PE.unsqueeze(0)
-
+        enc_out = x
+        for e in self.encoder_blocks:
+            enc_out = e(enc_out)
+        dec_out = x
+        for d in self.decoder_blocks:
+            dec_out = d(dec_out, enc_out)
+        out = self.final_linear(out)
+        out = self.sm(out)
+        return out
+        
 
 
 
